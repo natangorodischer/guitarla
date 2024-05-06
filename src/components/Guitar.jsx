9822536014e2906f1d8831bfd6496566abd3d9f4
natan.gorodischer <natan.gorodischer@gmail.com>
2024-05-06 15:58:11 -0400

    add proyect

---
+++ src/components/Guitar.jsx
@@ -0,0 +1,34 @@
+
+
+// eslint-disable-next-line react/prop-types
+export const Guitar = ({ guitar, addToCart }) => {
+    // Verificar si el objeto guitar es indefinido
+    if (!guitar) {
+        return null; // Devolver null o renderizar un mensaje de marcador de posición/error
+    }
+
+    // Destructurar las propiedades del objeto guitar
+    // eslint-disable-next-line react/prop-types
+    const { name, image, description, price } = guitar;
+
+    return (
+        <div className="col-md-6 col-lg-4 my-4 row align-items-center">
+            <div className="col-4">
+                <img className="img-fluid" src={`/img/${image}.jpg`} alt="Imagen de guitarra" />
+            </div>
+            <div className="col-8">
+                <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
+                <p>{description}</p>
+                <p className="fw-bold text-primary fs-3">${price}</p>
+                <button
+                    type="button"
+                    className="btn btn-dark w-100"
+                    onClick={() => addToCart(guitar)}
+                >
+                    Agregar al Carrito
+                </button>
+            </div>
+        </div>
+    );
+};
+
